bed51d1c0eced7b394ace9fab01c57b386d1b8d5
annaayeon <ayeon52@kookmin.ac.kr>
2023-09-22 16:25:51 +0900

    .

--- color.py
+++ color.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 print("Frame size : ", frame_size)
